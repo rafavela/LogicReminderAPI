af4cc241f428de3bb479025614fbb973e7c2fa3c
Rafael Velazquez Carmona <rafavela01@Rafaels-MacBook-Air.local>
2021-11-27 15:05:57 -0600

    random id generation added

--- src/main/java/com/dragonfruit/service/LogicReminderService.java
+++ src/main/java/com/dragonfruit/service/LogicReminderService.java
@@ -1,19 +1,29 @@
 package com.dragonfruit.service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.dragonfruit.bean.ReminderBean;
 
 @Service
+@ConfigurationProperties(prefix="reminder")
 public class LogicReminderService {
+	
+	private String phraseEndpoint;
+	private String countEndpoint;
+	private Integer minInteger;
 	
 	@Autowired
 	private WebClient.Builder webClientBuilder;	
 	
-	public ReminderBean getReminderBean() {		
-		ReminderBean reminderBean=this.getMono("http://localhost:8082/reminder/v1?id=1", ReminderBean.class);
+	public ReminderBean getReminderBean() {	
+		Long count = this.countReminder();
+		int randomId=this.getRandomId(count);
+		ReminderBean reminderBean=this.getMono(phraseEndpoint+randomId, ReminderBean.class);
 		
 		return reminderBean;
 	}
@@ -25,5 +35,27 @@ public class LogicReminderService {
 				.retrieve()
 				.bodyToMono(scienceFictionObject)
 				.block();		
-	}	
+	}
+
+	private Long countReminder() {
+		return this.getMono(countEndpoint, Long.class);
+	}
+	
+	private int getRandomId(Long max) {
+		return ThreadLocalRandom.current()
+				.nextInt(minInteger, 
+						max.intValue() + 1);
+	}
+	
+	public void setPhraseEndpoint(String phraseEndpoint) {
+		this.phraseEndpoint = phraseEndpoint;
+	}
+
+	public void setCountEndpoint(String countEndpoint) {
+		this.countEndpoint = countEndpoint;
+	}
+
+	public void setMinInteger(Integer minInteger) {
+		this.minInteger = minInteger;
+	}
 }
