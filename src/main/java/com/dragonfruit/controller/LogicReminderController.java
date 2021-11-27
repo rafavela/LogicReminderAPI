421cb5c723ae3c64f7183a09c3b84ffc06014086
Rafael Velazquez Carmona <rafavela01@Rafaels-MacBook-Air.local>
2021-11-27 17:12:29 -0600

    post reminder added

--- src/main/java/com/dragonfruit/controller/LogicReminderController.java
+++ src/main/java/com/dragonfruit/controller/LogicReminderController.java
@@ -2,6 +2,8 @@ package com.dragonfruit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class LogicReminderController {
 	@GetMapping
 	public ReminderBean doGet(){
 		return logicReminderService.getReminderBean();
+	}
+
+	@PostMapping
+	public ReminderBean saveReminder(@RequestBody ReminderBean reminderBean) {
+		return logicReminderService.saveReminderBean(reminderBean);
 	}
 }
--- src/main/java/com/dragonfruit/service/LogicReminderService.java
+++ src/main/java/com/dragonfruit/service/LogicReminderService.java
@@ -9,13 +9,21 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import com.dragonfruit.bean.ReminderBean;
 
+import lombok.Setter;
+import reactor.core.publisher.Mono;
+
 @Service
 @ConfigurationProperties(prefix="reminder")
 public class LogicReminderService {
 	
+	@Setter
 	private String phraseEndpoint;
+	@Setter	
 	private String countEndpoint;
+	@Setter	
 	private Integer minInteger;
+	@Setter
+	private String saveEndpont;
 	
 	@Autowired
 	private WebClient.Builder webClientBuilder;	
@@ -28,15 +36,31 @@ public class LogicReminderService {
 		return reminderBean;
 	}
 	
-	private <T> T getMono(String uri,Class<T> scienceFictionObject) {
+	public ReminderBean saveReminderBean(ReminderBean reminderBean) {
+		return this.saveMono(saveEndpont, reminderBean);
+	}
+	
+	private <T> T getMono(String uri,Class<T> responseType) {
 		return webClientBuilder.build()
 				.get()
 				.uri(uri)
 				.retrieve()
-				.bodyToMono(scienceFictionObject)
+				.bodyToMono(responseType)
 				.block();		
 	}
 
+	private ReminderBean saveMono(String uri,ReminderBean reminderBean){
+		System.out.println("reminderBean "+reminderBean);
+		return webClientBuilder.build()
+				.post()
+				.uri(uri)
+				.body(Mono.just(reminderBean),ReminderBean.class)
+				.retrieve()
+				.bodyToMono(ReminderBean.class)
+				.block();
+				
+	}
+	
 	private Long countReminder() {
 		return this.getMono(countEndpoint, Long.class);
 	}
@@ -45,17 +69,5 @@ public class LogicReminderService {
 		return ThreadLocalRandom.current()
 				.nextInt(minInteger, 
 						max.intValue() + 1);
-	}
-	
-	public void setPhraseEndpoint(String phraseEndpoint) {
-		this.phraseEndpoint = phraseEndpoint;
-	}
-
-	public void setCountEndpoint(String countEndpoint) {
-		this.countEndpoint = countEndpoint;
-	}
-
-	public void setMinInteger(Integer minInteger) {
-		this.minInteger = minInteger;
 	}
 }
