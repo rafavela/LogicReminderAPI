eaec45d092c6cbeb84c0a72ac7f06704867a7800
Rafael Velazquez Carmona <rafavela01@Rafaels-MacBook-Air.local>
2021-11-28 00:59:44 -0600

    Adding eureka client and consume the registered services

--- src/main/java/com/dragonfruit/bean/ReminderBean.java
+++ src/main/java/com/dragonfruit/bean/ReminderBean.java
@@ -2,9 +2,11 @@ package com.dragonfruit.bean;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReminderBean {	
 	private Long reminderId;
 	private String englishExpression;
